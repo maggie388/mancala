df09f6b078b55feaf40a506f6798e16f5ad5d6f8
maggie388 <rodriguesmargarida@hotmail.com>
2021-02-28 20:09:15 -0500

    Fix styling for score

--- client/src/components/TurnTracker/TurnTracker.js
+++ client/src/components/TurnTracker/TurnTracker.js
@@ -8,17 +8,13 @@ const TurnTracker = ({ currentPlayer, gameInProgress, message, finalScore }) => 
                     <h3 className='turn-tracker__player-heading'>player one</h3>
                     <div>maggie</div>
                 </div>
-                <div className='turn-tracker__score'>
-                    {!gameInProgress && finalScore[0]}
-                </div>
+                {!gameInProgress && <div className='turn-tracker__score'>{finalScore[0]}</div>}
             </div>
             <div className='turn-tracker__message'>
                 {message}
             </div>
             <div className={currentPlayer === 'playerTwo' && gameInProgress ? 'turn-tracker__player--active' : 'turn-tracker__player'}>
-                <div className='turn-tracker__score'>
-                    {!gameInProgress && finalScore[1]}
-                </div>
+                {!gameInProgress && <div className='turn-tracker__score'>{finalScore[1]}</div>}
                 <div className='turn-tracker__name-group'>
                     <h3 className='turn-tracker__player-heading'>player two</h3>
                     <p>jeff</p>
